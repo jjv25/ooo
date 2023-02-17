16eb723277051c07b3cac08a4ddf7839c03b48f1
JV25 <125074653+jjv25@users.noreply.github.com>
2023-02-17 18:38:31 +0300

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,30 +1,11 @@
-def roman_to_int(s:str)->int:
-    res=0
-    d={'CM':900, 'CD':400, 'XC': 90, 'XL': 40, 'IX': 9, 'IV':4,
-        'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-    for k in d:
-        res+=d[k]*s.count(k)
-        s=s.replace(k, '')
-    return res
+from roman import *
 
-def int_to_roman(num:int)->str:
-    info=['I', 'X', 'C', 'M']
-    #      0    1    2    3   
-    weird_number=''# 58 LVIII XXXXXIIIIIIII
-    i=0
-    while num != 0:
-        n=num%10
-        weird_number=info[i]*n+weird_number
-        num//=10
-        i+=1
-    res=weird_number
-    res=res.replace('IIIIIIIII','IX')
-    res=res.replace('IIIII', 'V')
-    res=res.replace('IIII', 'IV')
-    res=res.replace('XXXXXXXXX','XC')
-    res=res.replace('XXXXX','L')
-    res=res.replace('XXXX','XL')
-    res=res.replace('CCCCCCCCC','CM')
-    res=res.replace('CCCCC','D')
-    res=res.replace('CCCC','CD')
-    return res
+ar = ['IV', 'LVIII', 'MCMXCIV', 'XCIX', 'LXXX', 'LXIX']
+print(ar) #выводим результат перевода
+for i in ar:
+    print(i, '->' ,roman_to_int(i)) #из арабских переносим в римские 
+
+ob=[4, 58, 1994, 26, 99, 69, 80]
+for i in ob:
+    print(i, '->',int_to_roman(i)) #из римских в арабские
+
